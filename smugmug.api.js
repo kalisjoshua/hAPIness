32c07e3823aa888698a2f4e790c18f089d9cf7ba
Joshua T Kalis <kalisjoshua@gmail.com>
2012-12-31 18:31:09 -0500

    added feature to inject the param method

--- smugmug.api.js
+++ smugmug.api.js
@@ -1,12 +1,246 @@
 /*jshint laxcomma:true*/
 /*global module*/
-(function (ajax, param) {
+(function () {
   "use strict";
 
-  function SmugmugAPI (key, version, endpoint) {
+  function SmugmugAPIHelper (key, version, endpoint) {
     if (!key || !version) {
       throw "An API key and valid version must be supplied to use the Smugmug API.";
     }
+
+    this.endpoint = endpoint || "json";
+    this.APIKey   = key;
+    this.version  = version;
+    this.url = "http{secure}://api.smugmug.com/services/api/{endpoint}/{version}/?{params}&JSONCallback=?"
+      .replace("{endpoint}", this.endpoint)
+      .replace("{version}", this.version);
+  }
+
+  SmugmugAPIHelper.prototype = {
+    setParamFn: function (fn) {
+      this.param = fn;
+
+      return this;
+    }
+    ,setSession: function (id) {
+      if (!id) {
+        throw "A SessionID must be passed in to set it on the object";
+      }
+
+      this.SessionID = id;
+
+      return this;
+    }
+  };
+
+  function configForCallToAPI (method, params, https) {
+    /*jshint validthis:true */
+    params = params || {};
+    params.method = method;
+    params.APIKey = params.APIKey || this.APIKey;
+
+    if (this.SessionID && !params.SessionID) {
+      params.SessionID = this.SessionID;
+    }
+    
+    if (this.param) {
+      return this.url
+        .replace("{secure}", (!!https ? "s" : ""))
+        .replace("{params}", this.param(params));
+    } else {
+      return {
+        "params": params
+        ,"url": this.url
+          .replace("{secure}", (!!https ? "s" : ""))
+      };
+    }
+  }
+
+  function builder (key, version) {
+    var helper = new SmugmugAPIHelper(key, version)
+      , versions = {
+        "1.2.2": [
+          "albums.applyWatermark"
+          ,"albums.changeSettings"
+          ,"albums.comments.add"
+          ,"albums.comments.get"
+          ,"albums.create"
+          ,"albums.delete"
+          ,"albums.get"
+          ,"albums.getInfo"
+          ,"albums.getStats"
+          ,"albums.removeWatermark"
+          ,"albums.reSort"
+          ,"albumtemplates.changeSettings"
+          ,"albumtemplates.create"
+          ,"albumtemplates.delete"
+          ,"albumtemplates.get"
+          ,"auth.checkAccessToken"
+          ,"auth.getAccessToken"
+          ,"auth.getRequestToken"
+          ,"categories.create"
+          ,"categories.delete"
+          ,"categories.get"
+          ,"categories.rename"
+          ,"communities.get"
+          ,"coupons.create"
+          ,"coupons.get"
+          ,"coupons.getInfo"
+          ,"coupons.modify"
+          ,"coupons.restrictions.albums.add"
+          ,"coupons.restrictions.albums.remove"
+          ,"family.add"
+          ,"family.get"
+          ,"family.remove"
+          ,"family.removeAll"
+          ,"fans.get"
+          ,"featured.albums.get"
+          ,"friends.add"
+          ,"friends.get"
+          ,"friends.remove"
+          ,"friends.removeAll"
+          ,"images.applyWatermark"
+          ,"images.changePosition"
+          ,"images.changeSettings"
+          ,"images.collect"
+          ,"images.comments.add"
+          ,"images.comments.get"
+          ,"images.crop"
+          ,"images.delete"
+          ,"images.get"
+          ,"images.getEXIF"
+          ,"images.getInfo"
+          ,"images.getStats"
+          ,"images.getURLs"
+          ,"images.removeWatermark"
+          ,"images.rotate"
+          ,"images.uploadFromURL"
+          ,"images.zoomThumbnail"
+          ,"login.anonymously"
+          ,"login.withHash"
+          ,"login.withPassword"
+          ,"logout"
+          ,"printmarks.create"
+          ,"printmarks.delete"
+          ,"printmarks.get"
+          ,"printmarks.getInfo"
+          ,"printmarks.modify"
+          ,"products.get  deprecated"
+          ,"service.ping"
+          ,"sharegroups.albums.add"
+          ,"sharegroups.albums.get"
+          ,"sharegroups.albums.remove"
+          ,"sharegroups.create"
+          ,"sharegroups.delete"
+          ,"sharegroups.get"
+          ,"sharegroups.getInfo"
+          ,"sharegroups.modify"
+          ,"styles.getTemplates"
+          ,"subcategories.create"
+          ,"subcategories.delete"
+          ,"subcategories.get"
+          ,"subcategories.getAll"
+          ,"subcategories.rename"
+          ,"themes.get"
+          ,"users.getInfo"
+          ,"users.getStats"
+          ,"users.getTree"
+          ,"watermarks.changeSettings"
+          ,"watermarks.create"
+          ,"watermarks.delete"
+          ,"watermarks.get"
+          ,"watermarks.getInfo"
+        ]
+        ,"1.3.0": [
+          "accounts.browse"
+          ,"albums.applyWatermark"
+          ,"albums.browse"
+          ,"albums.changeSettings"
+          ,"albums.comments.add"
+          ,"albums.comments.get"
+          ,"albums.create"
+          ,"albums.delete"
+          ,"albums.get"
+          ,"albums.getInfo"
+          ,"albums.getStats"
+          ,"albums.removeWatermark"
+          ,"albums.reSort"
+          ,"albumtemplates.changeSettings"
+          ,"albumtemplates.create"
+          ,"albumtemplates.delete"
+          ,"albumtemplates.get"
+          ,"auth.checkAccessToken"
+          ,"auth.getAccessToken"
+          ,"auth.getRequestToken"
+          ,"categories.create"
+          ,"categories.delete"
+          ,"categories.get"
+          ,"categories.rename"
+          ,"communities.get"
+          ,"coupons.create"
+          ,"coupons.get"
+          ,"coupons.getInfo"
+          ,"coupons.modify"
+          ,"coupons.restrictions.albums.add"
+          ,"coupons.restrictions.albums.remove"
+          ,"family.add"
+          ,"family.get"
+          ,"family.remove"
+          ,"family.removeAll"
+          ,"fans.get"
+          ,"featured.albums.get"
+          ,"friends.add"
+          ,"friends.get"
+          ,"friends.remove"
+          ,"friends.removeAll"
+          ,"images.applyWatermark"
+          ,"images.changePosition"
+          ,"images.changeSettings"
+          ,"images.collect"
+          ,"images.comments.add"
+          ,"images.comments.get"
+          ,"images.crop"
+          ,"images.delete"
+          ,"images.get"
+          ,"images.getEXIF"
+          ,"images.getInfo"
+          ,"images.getStats"
+          ,"images.getURLs"
+          ,"images.removeWatermark"
+          ,"images.rotate"
+          ,"images.uploadFromURL"
+          ,"images.zoomThumbnail"
+          ,"printmarks.create"
+          ,"printmarks.delete"
+          ,"printmarks.get"
+          ,"printmarks.getInfo"
+          ,"printmarks.modify"
+          ,"service.ping"
+          ,"sharegroups.albums.add"
+          ,"sharegroups.albums.get"
+          ,"sharegroups.albums.remove"
+          ,"sharegroups.browse"
+          ,"sharegroups.create"
+          ,"sharegroups.delete"
+          ,"sharegroups.get"
+          ,"sharegroups.getInfo"
+          ,"sharegroups.modify"
+          ,"styles.getTemplates"
+          ,"subcategories.create"
+          ,"subcategories.delete"
+          ,"subcategories.get"
+          ,"subcategories.getAll"
+          ,"subcategories.rename"
+          ,"themes.get"
+          ,"users.getInfo"
+          ,"users.getStats"
+          ,"users.getTree"
+          ,"watermarks.changeSettings"
+          ,"watermarks.create"
+          ,"watermarks.delete"
+          ,"watermarks.get"
+          ,"watermarks.getInfo"]
+        };
 
     if (versions[version]) {
       versions[version]
@@ -16,228 +250,18 @@
 
           namespace.reduce(function (acc, part) {
             return (acc[part] = acc[part] || {});
-          }, acc)[last] = callAPI.bind(this, "" + fn);
+          }, acc)[last] = configForCallToAPI.bind(helper, "smugmug." + fn);
 
           return acc;
-        }, this);
+        }, helper);
     } else {
       throw "Version '{version}' not available in [{keys}]."
         .replace("{version}", version)
         .replace("{keys}", Object.keys(versions).join(","));
     }
 
-    this.endpoint = endpoint || "json";
-    this.key = key;
-    this.version = version;
-
-    this.url = "http{secure}://api.smugmug.com/services/api/{endpoint}/{version}/?{params}&JSONCallback=?"
-      .replace("{endpoint}", this.endpoint)
-      .replace("{version}", this.version);
+    return helper;
   }
 
-  function callAPI (method, params, callback, https) {
-    /*jshint validthis:true */
-    params.method = method;
-    params.APIKey = params.APIKey || this.APIKey;
-    params.SessionID = params.SessionID || this.SessionID;
-    
-    ajax({
-      "data": null
-      ,"dataType": "jsonp"
-      ,"success": callback
-      ,"url": this.url
-        .replace("{secure}", (!!https ? "s" : ""))
-        .replace("{params}", param(params))
-    });
-  }
-
-  function Smugmug (key, version) {
-    return new SmugmugAPI(key, version);
-  }
-
-  var versions = {
-    "1.2.2": [
-      "albums.applyWatermark"
-      ,"albums.changeSettings"
-      ,"albums.comments.add"
-      ,"albums.comments.get"
-      ,"albums.create"
-      ,"albums.delete"
-      ,"albums.get"
-      ,"albums.getInfo"
-      ,"albums.getStats"
-      ,"albums.removeWatermark"
-      ,"albums.reSort"
-      ,"albumtemplates.changeSettings"
-      ,"albumtemplates.create"
-      ,"albumtemplates.delete"
-      ,"albumtemplates.get"
-      ,"auth.checkAccessToken"
-      ,"auth.getAccessToken"
-      ,"auth.getRequestToken"
-      ,"categories.create"
-      ,"categories.delete"
-      ,"categories.get"
-      ,"categories.rename"
-      ,"communities.get"
-      ,"coupons.create"
-      ,"coupons.get"
-      ,"coupons.getInfo"
-      ,"coupons.modify"
-      ,"coupons.restrictions.albums.add"
-      ,"coupons.restrictions.albums.remove"
-      ,"family.add"
-      ,"family.get"
-      ,"family.remove"
-      ,"family.removeAll"
-      ,"fans.get"
-      ,"featured.albums.get"
-      ,"friends.add"
-      ,"friends.get"
-      ,"friends.remove"
-      ,"friends.removeAll"
-      ,"images.applyWatermark"
-      ,"images.changePosition"
-      ,"images.changeSettings"
-      ,"images.collect"
-      ,"images.comments.add"
-      ,"images.comments.get"
-      ,"images.crop"
-      ,"images.delete"
-      ,"images.get"
-      ,"images.getEXIF"
-      ,"images.getInfo"
-      ,"images.getStats"
-      ,"images.getURLs"
-      ,"images.removeWatermark"
-      ,"images.rotate"
-      ,"images.uploadFromURL"
-      ,"images.zoomThumbnail"
-      ,"login.anonymously"
-      ,"login.withHash"
-      ,"login.withPassword"
-      ,"logout"
-      ,"printmarks.create"
-      ,"printmarks.delete"
-      ,"printmarks.get"
-      ,"printmarks.getInfo"
-      ,"printmarks.modify"
-      ,"products.get  deprecated"
-      ,"service.ping"
-      ,"sharegroups.albums.add"
-      ,"sharegroups.albums.get"
-      ,"sharegroups.albums.remove"
-      ,"sharegroups.create"
-      ,"sharegroups.delete"
-      ,"sharegroups.get"
-      ,"sharegroups.getInfo"
-      ,"sharegroups.modify"
-      ,"styles.getTemplates"
-      ,"subcategories.create"
-      ,"subcategories.delete"
-      ,"subcategories.get"
-      ,"subcategories.getAll"
-      ,"subcategories.rename"
-      ,"themes.get"
-      ,"users.getInfo"
-      ,"users.getStats"
-      ,"users.getTree"
-      ,"watermarks.changeSettings"
-      ,"watermarks.create"
-      ,"watermarks.delete"
-      ,"watermarks.get"
-      ,"watermarks.getInfo"
-    ]
-    ,"1.3.0": [
-      "accounts.browse"
-      ,"albums.applyWatermark"
-      ,"albums.browse"
-      ,"albums.changeSettings"
-      ,"albums.comments.add"
-      ,"albums.comments.get"
-      ,"albums.create"
-      ,"albums.delete"
-      ,"albums.get"
-      ,"albums.getInfo"
-      ,"albums.getStats"
-      ,"albums.removeWatermark"
-      ,"albums.reSort"
-      ,"albumtemplates.changeSettings"
-      ,"albumtemplates.create"
-      ,"albumtemplates.delete"
-      ,"albumtemplates.get"
-      ,"auth.checkAccessToken"
-      ,"auth.getAccessToken"
-      ,"auth.getRequestToken"
-      ,"categories.create"
-      ,"categories.delete"
-      ,"categories.get"
-      ,"categories.rename"
-      ,"communities.get"
-      ,"coupons.create"
-      ,"coupons.get"
-      ,"coupons.getInfo"
-      ,"coupons.modify"
-      ,"coupons.restrictions.albums.add"
-      ,"coupons.restrictions.albums.remove"
-      ,"family.add"
-      ,"family.get"
-      ,"family.remove"
-      ,"family.removeAll"
-      ,"fans.get"
-      ,"featured.albums.get"
-      ,"friends.add"
-      ,"friends.get"
-      ,"friends.remove"
-      ,"friends.removeAll"
-      ,"images.applyWatermark"
-      ,"images.changePosition"
-      ,"images.changeSettings"
-      ,"images.collect"
-      ,"images.comments.add"
-      ,"images.comments.get"
-      ,"images.crop"
-      ,"images.delete"
-      ,"images.get"
-      ,"images.getEXIF"
-      ,"images.getInfo"
-      ,"images.getStats"
-      ,"images.getURLs"
-      ,"images.removeWatermark"
-      ,"images.rotate"
-      ,"images.uploadFromURL"
-      ,"images.zoomThumbnail"
-      ,"printmarks.create"
-      ,"printmarks.delete"
-      ,"printmarks.get"
-      ,"printmarks.getInfo"
-      ,"printmarks.modify"
-      ,"service.ping"
-      ,"sharegroups.albums.add"
-      ,"sharegroups.albums.get"
-      ,"sharegroups.albums.remove"
-      ,"sharegroups.browse"
-      ,"sharegroups.create"
-      ,"sharegroups.delete"
-      ,"sharegroups.get"
-      ,"sharegroups.getInfo"
-      ,"sharegroups.modify"
-      ,"styles.getTemplates"
-      ,"subcategories.create"
-      ,"subcategories.delete"
-      ,"subcategories.get"
-      ,"subcategories.getAll"
-      ,"subcategories.rename"
-      ,"themes.get"
-      ,"users.getInfo"
-      ,"users.getStats"
-      ,"users.getTree"
-      ,"watermarks.changeSettings"
-      ,"watermarks.create"
-      ,"watermarks.delete"
-      ,"watermarks.get"
-      ,"watermarks.getInfo"]
-    };
-
-  typeof module === "undefined" ? this.Smugmug = Smugmug : module.exports = Smugmug;
+  typeof module === "undefined" ? this.smugmug = builder : module.exports = builder;
 }());
--- test/mocha.js
+++ test/mocha.js
@@ -4,50 +4,76 @@
 var assert = require("chai").assert
   , http = require("http")
   , key = (require("fs")).readFileSync("./api.key", "UTF-8")
-  , smugmug = require("../smugmug.api");
+  , qs = require("querystring")
+  , smugmug_builder = require("../smugmug.api")
+  , url_parser = require("url");
 
 describe("SmugmugAPI", function () {
   it("should be a function.", function () {
-    assert.isFunction(smugmug);
+    assert.isFunction(smugmug_builder);
   });
 
   it("should require arguments", function () {
-    assert.throws(smugmug, "No passed arguments is not valid.");
+    assert.throws(smugmug_builder, "No passed arguments is not valid.");
 
     assert.throws(function () {
-      smugmug("12345");
+      smugmug_builder("12345");
     }, "An API version must be supplied.");
     
     assert.throws(function () {
-      smugmug("12345", "1");
+      smugmug_builder("12345", "1");
     }, "The version passed must be valid.");
   });
 
   it("should not throw an error", function () {
     assert.doesNotThrow(function () {
-      smugmug("1234", "1.3.0");
+      smugmug_builder("1234", "1.3.0");
     }, "The version is valid even though the API key hasn't been tested.");
   });
 
   describe("Methods", function () {
     it("should have API version 1.3.0 methods defined.", function() {
-      var api = smugmug("1234", "1.3.0");
+      var api = smugmug_builder("1234", "1.3.0");
       assert(api.accounts.browse, "Smugmug.accounts.browse should exist");
       assert(api.sharegroups.albums.remove, "Smugmug.sharegroups.albums.remove should exist");
       assert(api.coupons.restrictions.albums.add, "Smugmug.coupons.restrictions.albums.add should exist");
     });
 
     it("should have API version 1.2.2 methods defined.", function() {
-      var api = smugmug("1234", "1.2.2");
+      var api = smugmug_builder("1234", "1.2.2");
       assert(api.albums.applyWatermark, "Smugmug.albums.applyWatermark should exist");
       assert(api.coupons.create, "Smugmug.coupons.create should exist");
       assert(api.login.anonymously, "Smugmug.login.anonymously should exist");
     });
   });
 
-  console.log(key);
+  describe("Parameterization configuration", function () {
+    var smugmug = smugmug_builder(key, "1.2.2");
+
+    it("should be null by default.", function () {
+      assert.isUndefined(smugmug.params, "No default parameterization provided.");
+    });
+
+    it("should be injectable.", function () {
+      assert(smugmug.setParamFn, "Set method for parameterization.");
+      assert.isUndefined(smugmug.param, "Default param method is undefined.");
+      smugmug.setParamFn(qs.stringify);
+      assert(smugmug.param, "Default param method is set.");
+    });
+
+    it("should now return a string as the request url and not an object with params.", function () {
+      assert.isUndefined(smugmug.service.ping().params, "String object does not have property 'params'.");
+      assert(/string/i.test({}.toString.call(smugmug.service.ping())), "Call should return a String.");
+    });
+  });
 
   // describe("Requests", function () {
+  //   var smugmug = smugmug_builder(key, "1.2.2")
+  //     , config = smugmug.service.ping()
+  //     , url = config.url
+  //       .replace("{params}", qs.stringify(config.params))
+  //     , request_obj = url_parser.parse(url);
+
   //   it("should get a response from a url.", function (done) {
   //     function complete (response) {
   //       var m = '';
@@ -64,8 +90,8 @@ describe("SmugmugAPI", function () {
 
   //     http
   //       .request({
-  //         host: "api.smugmug.com"
-  //         ,path: "/services/api/json/1.2.2/?method=smugmug.login.anonymously&APIKey=6C3JkTZdWzQjswrYpAMOUgBAhIkpJtTx&JSONCallback=?"
+  //         host: request_obj.host
+  //         ,path: request_obj.path
   //       }, complete)
   //       .end();
   //   });
